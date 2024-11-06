4682a9249af34f4342ecfa4e72c06a89afbea83b
JHKIMS <wjdgns5131@gmail.com>
2024-11-06 20:51:49 +0900

    feat: zod 에러 메시지 핸들링

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,22 +1,40 @@
-"use server"
-import {z} from "zod";
+"use server";
+import { z } from "zod";
 
-const formSchema = z.object({
-    username: z.string().min(3).max(10),
+const checkUsername = (username: string) => !username.includes("user");
+const checkPasswords = ({
+  password,
+  confirmPass,
+}: {
+  password: string;
+  confirmPass: string;
+}) => password === confirmPass;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({ invalid_type_error: "Username must be a string!" })
+      .min(3, "Way too short.")
+      .max(10, "That is too long.")
+      .refine(checkUsername, "No user allowed"),
     email: z.string().email(),
-    password: z.string().min(10),
+    password: z.string().min(7),
     confirmPass: z.string().min(10),
-})
+  })
+  .refine(checkPasswords, {
+    message: "Both passwords should be the same!",
+    path: ["confirmPass"],
+  });
 
-export async function createAccount(prevState: any, formData:FormData){
-    const data = {
-        username: formData.get("username"),
-        email: formData.get("email"),
-        password: formData.get("password"),
-        confirmPass: formData.get("confirmPass"),
-    };
-    const result = formSchema.safeParse(data);
-    if(!result.success){
-        return result.error.flatten();
-    }
+export async function createAccount(prevState: any, formData: FormData) {
+  const data = {
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirmPass: formData.get("confirmPass"),
+  };
+  const result = formSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  }
 }

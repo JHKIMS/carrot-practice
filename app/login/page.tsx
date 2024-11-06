c4df793e9bfb7555937af0fced5b7fc72d0ef6bc
JHKIMS <wjdgns5131@gmail.com>
2024-11-06 09:15:17 +0900

    feat: ServerAction을 이용한 handler "use server"

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,26 +3,32 @@ import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 const Login = () => {
+  const handleForm = async (formData: FormData) => {
+    "use server"; // 이 함수는 서버에서만 실행되도록.
+    console.log(formData.get("email"));
+  };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">Hi</h1>
         <h2 className="text-xl">Login with email and password</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
-            type="email"
-            placeholder="Email"
-            required
-            errors={[]}
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={[]}
         />
         <FormInput
-            type="password"
-            placeholder="password"
-            required
-            errors={[]}
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          errors={[]}
         />
-        <FormBtn loading={false} text="Crate Account"/>
+        <FormBtn loading={false} text="Login" />
       </form>
       <SocialLogin />
     </div>
